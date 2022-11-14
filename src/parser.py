a51a575ac41e0d32065ce8d34e7cfa9af7ba5054
Mas281 <github@samdev.io>
2022-11-14 13:15:18 +0000

    Change parsing file structure

--- src/parser.py
+++ src/parser.py
@@ -85,11 +85,14 @@ def perform_glob_expansion(raw_command: str) -> List[str]:
 
     return tokens
 
+
+
 from antlr4 import CommonTokenStream, ParseTreeWalker
 
-from CommandLexer import CommandLexer
-from CommandParser import CommandParser
-from CommandListener import CommandListener
+from parsing.CommandLexer import CommandLexer
+from parsing.CommandParser import CommandParser
+from parsing.CommandListener import CommandListener
+
 
 def parseTreeWalker(input: str):
 
@@ -98,9 +101,11 @@ def parseTreeWalker(input: str):
     parser = CommandParser(tokens)
     tree = parser.command()
 
-    listener = newCommandListener()
-    walker = ParseTreeWalker()
-    walker.walk(listener, tree)
+
+
+    # listener = newCommandListener()
+    # walker = ParseTreeWalker()
+    # walker.walk(listener, tree)
 
 
 class newCommandListener(CommandListener):
--- src/shell.py
+++ src/shell.py
@@ -43,4 +43,7 @@ def handle_input(cmd_line: str):
 
 
 if __name__ == "__main__":
+    while True:
+        i = input()
+        parser.parseTreeWalker(i)
     run_shell()
