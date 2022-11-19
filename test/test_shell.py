01e816be287a13cf06c7c29bd613801e96beba8c
Ramit Bag <ramit.bag.710@gmail.com>
2022-11-19 19:27:19 +0000

    .

--- test/test_shell.py
+++ test/test_shell.py
@@ -3,6 +3,10 @@ from collections import deque
 
 from shell import evaluate
 
+from src.applications.echo import Echo
+from src.applications.cat import Cat
+
+
 
 class TestShell(unittest.TestCase):
     def test_shell(self):
@@ -10,6 +14,18 @@ class TestShell(unittest.TestCase):
         evaluate("echo foo", out)
         self.assertEqual(out.popleft(), "foo\n")
         self.assertEqual(len(out), 0)
+
+
+    def test_echo(self):
+        self.assertEqual(Echo.help_message(),"echo [args...]")
+
+    def test_cat(self):
+        self.assertEqual(Cat.help_message(),"cat [files...]")
+
+
+
+
+
 
 
 if __name__ == "__main__":
