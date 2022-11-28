f5871662731e347ebdb710d1f158735874c7847a
Ramit Bag <ramit.bag.710@gmail.com>
2022-11-28 06:50:19 +0000

    Update test_shell.py

--- test/test_shell.py
+++ test/test_shell.py
@@ -2,15 +2,17 @@ import unittest
 from collections import deque
 from src.shell import evaluate
 from src.parser import ParsingError
+from src.applications.application import ApplicationError
+
 
 class TestShell(unittest.TestCase):
-    #def test_shell_empty(self):
+    # def test_shell_empty(self):
     #    out = deque()
     #    self.assertRaises(ParsingError, evaluate, "", out)
 
     def test_shell_invalid_command(self):
         out = deque()
-        self.assertRaises(ParsingError, evaluate, "???", out)
+        self.assertRaises(ApplicationError, evaluate, "???", out)
 
 
 if __name__ == "__main__":
