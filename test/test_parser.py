155c82661e0ed85dbf385ef2eea71e7e0be12291
Mas281 <github@samdev.io>
2022-11-29 10:59:20 +0000

    Remove duplicate tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -64,36 +64,3 @@ class TestParser(unittest.TestCase):
             out = execute_command(f"echo `cat {tmp.name}`")
             expected = ["Hello World Goodbye World\n"]
             self.assertEqual(out, expected)
-
-    def test_double_quoted(self):
-        out = execute_command("echo \"foo bar\"")
-        expected = ["foo bar\n"]
-        self.assertEqual(out, expected)
-
-    def test_double_quoted_invalid(self):
-        with self.assertRaises(ParsingError):
-            execute_command("echo \"\n'\"")
-
-    def test_back_quoted(self):
-        out = execute_command("echo `echo foo bar`")
-        expected = ["foo bar\n"]
-        self.assertEqual(out, expected)
-
-    def test_back_quoted_invalid(self):
-        with self.assertRaises(ParsingError):
-            execute_command("```")
-
-    def test_back_quoted_within_double_quotes(self):
-        out = execute_command("echo \"foo `echo bar`\"")
-        expected = ["foo bar\n"]
-        self.assertEqual(out, expected)
-
-    def test_arg_splitting(self):
-        with tempfile.NamedTemporaryFile(mode="a") as tmp:
-            tmp.write("Hello World\n")
-            tmp.write("Goodbye World\n")
-            tmp.flush()
-
-            out = execute_command(f"echo `cat {tmp.name}`")
-            expected = ["Hello World Goodbye World\n"]
-            self.assertEqual(out, expected)
